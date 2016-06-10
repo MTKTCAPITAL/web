dddfb3de823b70ee4b34585d6fdf610388c4aad6
Zachary Nguyen <zac@nhulai.com>
2016-06-10 15:07:42 -0700

    convert to laravel

--- resources/views/template/app.blade.php
+++ resources/views/template/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css">
-    <link type="text/css" rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="{{elixir('css/app.css')}}">
     <script type="text/javascript" src="js/jquery-2.2.4.min.js"></script>
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
 </head>
